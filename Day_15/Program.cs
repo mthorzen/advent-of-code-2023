b94f683704c4f8b2194a487d7302a2742ee29433
Michael Thorzén <michael.thorzen@nordoba.com>
2023-12-15 11:19:10 +0100

    Day 15 part 1

---
+++ Day_15/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tools;
+
+bool example = false;
+var line = FileReader.ReadLines(example ? "Input_example.txt" : "Input.txt", false)[0];
+var sequences = line.Split(",");
+
+int Hash(string str)
+{
+    int v = 0;
+    for (var i = 0; i < str.Length; i++)
+    {
+        v += str[i];
+        v = (v * 17) % 256;
+    }
+    return v;
+}
+
+long sum = 0;
+for (var i = 0; i < sequences.Length; i++)
+{
+    sum += Hash(sequences[i]);
+}
+
+Console.WriteLine("Sum: " + sum);
